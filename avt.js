ab08739c1fcad6a95297cb0c23bca390a494d1a9
Muhammad Salman Abid <muhsalmanabid@gmail.com>
2023-02-28 14:25:05 +0700

    fix: retry with proxy

--- avt.js
+++ avt.js
@@ -2,7 +2,9 @@ const axios = require('axios');
 const qs = require('qs');
 const fs = require('fs');
 const { updateWallet } = require('./wallet');
-const SocksAgent = require('axios-socks5-agent')
+// const SocksAgent = require('axios-socks5-agent');
+const HttpsProxyAgent = require('https-proxy-agent');
+const UrlParse = require('url-parse');
 
 const retryClaim = async (dataWallet) => {
     console.log('start retry')
@@ -19,84 +21,80 @@ const retryClaim = async (dataWallet) => {
     });
     const proxyList = JSON.parse(fileData);
     for(let proxy of proxyList){
-        if(proxy.protocols[0] === 'socks4' || !proxy.protocols.length || !proxy.ip || !proxy.port){
-            console.log('proxy protocols or format not valid!');
-        } else {
-            let config = {
-                method: 'post',
-                maxBodyLength: Infinity,
-                url: 'https://beta-faucet-service.avt.trade/airdropOpAvt',
-                headers: { 
-                        'authority': 'beta-faucet-service.avt.trade', 
-                        'accept': 'application/json, text/plain, */*', 
-                        'accept-language': 'en-US,en;q=0.9', 
-                        'content-type': 'application/x-www-form-urlencoded', 
-                        'origin': 'https://beta-water.avt.trade', 
-                        'referer': 'https://beta-water.avt.trade/', 
-                        'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"', 
-                        'sec-ch-ua-mobile': '?0', 
-                        'sec-ch-ua-platform': '"Windows"', 
-                        'sec-fetch-dest': 'empty', 
-                        'sec-fetch-mode': 'cors', 
-                        'sec-fetch-site': 'same-site', 
-                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
-                },
-                json: true,
-                data : data
-            };
-
-            if(proxy.protocols[0] === 'socks5'){
-                const { httpAgent, httpsAgent } = SocksAgent({
-                    agentOptions: {
-                      keepAlive: true,
+        if(!model.success){
+            if(!proxy.protocols[0] === 'http' || !proxy.protocols[0] === 'https' || !proxy.protocols.length || !proxy.ip || !proxy.port){
+                console.log('proxy protocols or format not valid!');
+            } else {
+                let proxyOpts = UrlParse(`${proxy.protocols[0]}://${proxy.ip}:${proxy.port}`);
+                proxyOpts.auth = `${proxy.username}:${proxy.password}`;
+                let config = {
+                    method: 'post',
+                    maxBodyLength: Infinity,
+                    url: 'https://beta-faucet-service.avt.trade/airdropOpAvt',
+                    headers: { 
+                            'authority': 'beta-faucet-service.avt.trade', 
+                            'accept': 'application/json, text/plain, */*', 
+                            'accept-language': 'en-US,en;q=0.9', 
+                            'content-type': 'application/x-www-form-urlencoded', 
+                            'origin': 'https://beta-water.avt.trade', 
+                            'referer': 'https://beta-water.avt.trade/', 
+                            'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"', 
+                            'sec-ch-ua-mobile': '?0', 
+                            'sec-ch-ua-platform': '"Windows"', 
+                            'sec-fetch-dest': 'empty', 
+                            'sec-fetch-mode': 'cors', 
+                            'sec-fetch-site': 'same-site', 
+                            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
                     },
-                    // socks5
-                    host: proxy.ip,
-                    port: proxy.port
-                });
-                config = {
-                    ...config,
-                    httpAgent,
-                    httpsAgent
-                }
-            } else {
-                config = {
-                    ...config,
-                    proxy: {
-                        protocol: proxy.protocols[0],
-                        host: proxy.ip,
-                        port: proxy.port
+                    proxy: false,
+                    httpsAgent: new HttpsProxyAgent(proxyOpts),
+                    json: true,
+                    data : data
+                };
+            
+                const response = await axios(config).catch(err => {
+                    if(err){
+                        if(err.response){
+                            if(err.response.statusText){
+                                console.log(err.response.statusText);
+                            } else {
+                                console.log(err.response);
+                            }
+                        } else {
+                            console.log(err);
+                        }
                     }
+                });
+                if(typeof response === 'object' && response.data && response.data.code === 200){
+                    dataWallet.isAlreadyClaim = true;
+                    dataWallet.data = dataWallet.data.data;
+                    model.data = dataWallet;
+                    console.log(`address ${dataWallet.address} success claim! with proxy ${proxy.protocols[0]}://${proxy.ip}:${proxy.port}`)
+                    break;
+                } else if(typeof response === 'object' && response.data && response.data.msg === 'This address has been picked up'){
+                    dataWallet.isAlreadyClaim = true;
+                    dataWallet.data = response.data.msg;
+                    model.data = dataWallet;
+                    console.log('failed claim! ' + dataWallet.address + ' already claimed! with proxy ${proxy.protocols[0]}://${proxy.ip}:${proxy.port}');
+                    break;
+                } else if(typeof response === 'object' && response.data && response.data.msg === 'The IP address has reached its limit'){
+                    console.log(`proxy ${proxy.protocols[0]} ${proxy.ip}:${proxy.port} failed!`)
+                    console.log(response.data);
+                } else {
+                    console.log(response);
                 }
-            }
-        
-            const response = await axios(config).catch(err => {
-                console.log(err);
-            });
-            if(response.data.code === 200){
-                dataWallet.isAlreadyClaim = true;
-                dataWallet.data = dataWallet.data.data;
-                model.data = dataWallet;
-                console.log(`address ${dataWallet.address} success claim!`)
-                break;
-            } else if(response.data.msg === 'This address has been picked up' || response.data.code === 1){
-                dataWallet.isAlreadyClaim = true;
-                dataWallet.data = response.data.data;
-                model.data = dataWallet;
-                console.log('failed claim! ' + dataWallet.address + ' already claimed!');
-                break;
-            } else if(response.data.code === 2 || response.data.msg === 'The IP address has reached its limit'){
-                console.log(`proxy ${proxy.protocols[0]} ${proxy.ip}:${proxy.port} failed!`)
-                console.log(response.data);
-            } else {
-                console.log(response);
             }
         }
+    }
+    if(model.success){
+        console.log('success retry claim!');
+    } else {
+        console.log('failed retry claim!');
     }
     return model;
 }
 
-const claim = async (walletList) => {
+const claim = async (walletList, retry) => {
     console.log('start claim faucet!');
     console.log('total wallet: ' + walletList.length);
     for(let datas of walletList){
@@ -140,14 +138,11 @@ const claim = async (walletList) => {
                 console.log('failed claim! ' + datas.address + ' already claimed!');
             } else if(response.data.code === 2 || response.data.msg === 'The IP address has reached its limit'){
                 console.log('limit! ganti ip sana');
-                //uncoment below code if you have good proxy
-                retryClaim(datas).then(datas => {
-                    if(datas.success){
-                        console.log('success retry claim!');
-                    }
-                })
+                if(retry){
+                    await retryClaim(datas);
+                }
             } else {
-
+                console.log('failed claim!')
             }
         }
     }
--- index.js
+++ index.js
@@ -5,11 +5,29 @@ const prompt = require('prompt-sync')();
 
 const option1 = async () => {
     const wallet = await createWallet();
-    await claim(wallet);
+    await claim(wallet, true);
     return 0;
 }
 
 const option2 = async () => {
+    const wallet = await createWallet();
+    await claim(wallet);
+    return 0;
+}
+
+const option3 = async () => {
+    const fileData = fs.readFileSync('wallet.json', (err) => {
+        if(err){
+            console.log('wallet not found!')
+            return 0;
+        }
+    })
+    const wallet = JSON.parse(fileData);
+    await claim(wallet, true);
+    return 0;
+}
+
+const option4 = async () => {
     const fileData = fs.readFileSync('wallet.json', (err) => {
         if(err){
             console.log('wallet not found!')
@@ -25,13 +43,17 @@ const mulai = async () => {
     let ulang = true;
     while(ulang){
         process.stdout.write('\033c');
-        console.log('1. Create 10 wallet and claim\n2. Claim faucet with address in wallet.json\n');
+        console.log('1. Create 10 wallet and claim with proxy\n2. Create 10 wallet and claim without proxy (manual)\n3. Claim faucet with address in wallet.json\n4. Claim faucet with address in wallet.json without proxy (manual)\n');
         let pilihan = prompt('Masukan pilihan mu (1 atau 2): ');
     
         if(pilihan === '1' || pilihan === 1){
             await option1();
-        } else if(pilihan === '2' || pilihan === 2){
-            await option2()
+        } if(pilihan === '2' || pilihan === 2){
+            await option2();
+        } else if(pilihan === '3' || pilihan === 3){
+            await option3()
+        } else if(pilihan === '4' || pilihan === 4){
+            await option4()
         } else {
             console.log('Sing jelas bro');
             process.exit();
